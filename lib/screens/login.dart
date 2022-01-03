cfcdab0e25353ea45190031190e1602416a9804f
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 10:30:49 +0300

    Update login.dart

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,6 +5,46 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(80.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                  'Welcome',
+                   style: Theme.of(context).textTheme.headline1,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Username',
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              ElevatedButton(
+                //Verilen rotayı iterek ve ardından yeni rota animasyonu tamamlandıktan sonra önceki rotayı atarak, verilen bağlamı en sıkı şekilde içine alan gezginin geçerli rotasını değiştirin.
+                //Yeni bir rotaya basıldığında mevcut rota içinde devam eden hareketler iptal edilir.
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/catalog');
+                },
+                child: const Text('ENTER'),
+                style: ElevatedButton.styleFrom(
+                   primary: Colors.yellow,
+                 ),
+              )
+            ],
+          ),
+        ),
+      ),
+    )
   }
 }
