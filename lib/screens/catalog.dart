6e741cecbf08db77b73bf7c2f7c6984f31252561
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 11:42:10 +0300

    Update catalog.dart

--- lib/screens/catalog.dart
+++ lib/screens/catalog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_shopper/models/catalog.dart';
 
 class Catalog extends StatelessWidget {
   const Catalog({Key? key}) : super(key: key);
@@ -8,7 +9,17 @@ class Catalog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          _AppBar(),
+          const SliverToBoxAdapter(
+              child: SizedBox(
+            height: 12,
+          )),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index) => _ListItem(index)),
+          )
+        ],
       ),
     );
   }
@@ -31,6 +42,39 @@ class _AppBar extends StatelessWidget {
           icon: const Icon(Icons.shopping_cart),
         )
       ],
+    );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final int index;
+  const _ListItem(this.index, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var item = context.select<CatalogModel, Item>(
+      (catalog) => catalog.getByPosition(index),
+    );
+
+    var textTheme = Theme.of(context).textTheme.headline6;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: LimitedBox(
+        maxHeight: 48,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 1,
+              child: Container(
+                color: item.color,
+              ),
+            ),
+            const SizedBox(width: 24),
+            Expanded(child: Text(item.name, style: textTheme),),
+            const SizedBox(width: 24,),
+          ],
+        ),
+      ),
     );
   }
 }
