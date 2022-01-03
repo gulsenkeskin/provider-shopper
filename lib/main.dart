2f3bd64d2db36afb70f31acc4325ef68a73c5735
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 10:30:42 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider_shopper/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Login();
   }
 }
