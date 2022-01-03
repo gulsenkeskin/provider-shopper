9d396b60dd3de963d740d8217d96018e0d82be1d
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 12:29:05 +0300

    added _CartList

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
+import 'package:provider_shopper/models/cart.dart';
 
 class Cart extends StatelessWidget {
   const Cart({Key? key}) : super(key: key);
@@ -20,11 +22,43 @@ class Cart extends StatelessWidget {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(32),
+                child: _CartList(),
               ),
             ),
+            const Divider(height: 4,color: Colors.black,),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _CartList extends StatelessWidget {
+  //const _CartList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    var itemNameStyle=Theme.of(context).textTheme.headline6;
+    // Bu, Cart Modelinin geçerli durumunu alır ve ayrıca CartModel dinleyicileri bilgilendirdiğinde (başka bir deyişle değiştiğinde) Flutter'a bu widget'ı yeniden oluşturmasını söyler.
+
+    var cart = context.watch<CartModel>();
+
+    return ListView.builder(
+        itemBuilder: (context,index)=> ListTile(
+          leading: const Icon(Icons.done),
+          trailing: IconButton(
+            icon: const Icon(Icons.remove_circle_outline),
+            onPressed: (){
+              cart.remove(cart.items[index]);
+            },
+          ),
+          title: Text(
+            cart.items[index].name,
+            style: itemNameStyle,
+          ),
+        ),
+        itemCount: cart.items.length,
     );
   }
 }
