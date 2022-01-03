2ecdc692a7b5672c5297f381b6ad091937bfcbb6
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 11:18:54 +0300

    routes update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_shopper/common/theme.dart';
+import 'package:provider_shopper/models/cart.dart';
+import 'package:provider_shopper/models/catalog.dart';
+import 'package:provider_shopper/screens/catalog.dart';
 import 'package:provider_shopper/screens/login.dart';
 
 void main() {
@@ -10,6 +16,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Login();
+    // MultiProvider kullanmak, birden çok nesne sağlarken kullanışlıdır.
+    return MultiProvider(
+      providers: [
+        //Bu örnek uygulamada, Katalog Modeli asla değişmez, bu yüzden basit bir Sağlayıcı
+        // yeterli.
+        Provider(create: (context) => CatalogModel()),
+        //Cart Modeli, Change Notifier Sağlayıcısının kullanılmasını gerektiren bir Change Notifier olarak uygulanır. Ayrıca, Cart Modeli Katalog Modeline bağlıdır, bu nedenle bir Proxy Sağlayıcısına ihtiyaç vardır.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Provider Demo',
+        theme: appTheme,
+        initialRoute: '/',
+        routes: {
+          '/':(context)=> const Login(),
+          '/catalog':(context)=> const Catalog(),
+         // '/cart':(context)=> const Cart(),
+        },
+
+      ),
+    );
   }
 }
