be8087e9dfeca47fea406502d8e203a8e62dded1
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 12:03:06 +0300

    add button eklendi

--- lib/screens/catalog.dart
+++ lib/screens/catalog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:provider_shopper/models/cart.dart';
 import 'package:provider_shopper/models/catalog.dart';
 
 class Catalog extends StatelessWidget {
@@ -70,11 +71,56 @@ class _ListItem extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 24),
-            Expanded(child: Text(item.name, style: textTheme),),
-            const SizedBox(width: 24,),
+            Expanded(
+              child: Text(item.name, style: textTheme),
+            ),
+            const SizedBox(
+              width: 24,
+            ),
+            _AddButton(item: item),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  final Item item;
+  const _AddButton({required this.item, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //Bağlama.select() yöntemi, bir modelin * parçasındaki* değişiklikleri dinlemenizi sağlar. İlgilendiğiniz parçayı "seçen" (yani döndüren) bir işlev tanımlarsınız ve modelin belirli bir kısmı değişmedikçe sağlayıcı paketi bu widget'ı yeniden oluşturmaz. Bu önemli performans iyileştirmelerine yol açabilir.
+    // Bu önemli performans iyileştirmelerine yol açabilir.
+
+    var isInCart = context.select<CartModel, bool>(
+      // Burada, yalnızca [öğenin] sepetin içinde olup olmadığıyla ilgileniyoruz.
+      (cart) => cart.items.contains(item),
+    );
+
+    return TextButton(
+      onPressed: isInCart
+          ? null
+          : () {
+              //Öğe sepette değilse, kullanıcının eklemesine izin veririz. Bağlam kullanıyoruz.read() burada, çünkü kullanıcı düğmeye her dokunduğunda geri arama yürütülür. Başka bir deyişle, yapı yönteminin dışında yürütülür.
+              var cart = context.read<CartModel>();
+              cart.add(item);
+            },
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return Theme.of(context).primaryColor;
+          }
+          return null; // Widget'ın varsayılanına ertele.
+        }),
+      ),
+      child: isInCart
+          ? const Icon(
+              Icons.check,
+              semanticLabel: 'ADDED',
+            )
+          : const Text('ADD'),
     );
   }
 }
