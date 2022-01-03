ba9c8c08b4728451214e75a94466ccf818881f17
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 12:41:32 +0300

    added _CartTotal

--- lib/screens/cart.dart
+++ lib/screens/cart.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/src/provider.dart';
+import 'package:provider/provider.dart';
 import 'package:provider_shopper/models/cart.dart';
 
 class Cart extends StatelessWidget {
@@ -26,6 +26,7 @@ class Cart extends StatelessWidget {
               ),
             ),
             const Divider(height: 4,color: Colors.black,),
+            _CartTotal()
           ],
         ),
       ),
@@ -59,6 +60,38 @@ class _CartList extends StatelessWidget {
           ),
         ),
         itemCount: cart.items.length,
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  //const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var hugeStyle=Theme.of(context).textTheme.headline1!.copyWith(fontSize: 48);
+    return SizedBox(
+      height: 200,
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Bir modelin değişikliğini dinlemenin başka bir yolu da Consumer widget'ını eklemektir. Bu widget otomatik olarak CartModel'idinleyecek ve her değişiklikte oluşturucusunu yeniden çalıştıracaktır. Önemli olan, bu oluşturma yöntemindeki widget'ların geri kalanını yeniden oluşturmamasıdır.
+            Consumer<CartModel>(
+                builder: (context, cart, child) =>
+                    Text('\$${cart.totalPrice}', style: hugeStyle)),
+            const SizedBox(width: 24),
+            TextButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Buying not supported yet.')));
+              },
+              style: TextButton.styleFrom(primary: Colors.white),
+              child: const Text('BUY'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
