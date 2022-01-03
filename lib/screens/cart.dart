b0c6c2dbe6d5be844a00a92b227f8ca11133f168
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-03 11:42:07 +0300

    Create cart.dart

---
+++ lib/screens/cart.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Cart',
+          style: Theme.of(context).textTheme.headline1,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Container(
+        color: Colors.yellow,
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
